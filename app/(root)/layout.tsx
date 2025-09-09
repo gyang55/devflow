042afe5ad80e1036b6ad5ff36c4f4c46f1ff746b
Guang Yang <guangy@emonster.ca>
2025-09-09 13:58:29 -0700

    github auth

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,18 @@
+import Navbar from "@/components/navigation/navbar";
+import { ReactNode } from "react";
+
+const RootLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <main className="background-light850_dark100 realtive">
+      <Navbar />
+
+      <div className="flex">
+        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
+          <div className="mx-auto w-full max-w-5xl">{children}</div>
+        </section>
+      </div>
+    </main>
+  );
+};
+
+export default RootLayout;
